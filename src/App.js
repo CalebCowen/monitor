54e735863032f7adb3ef5ed186d5bb05683b6ae9
Caleb <calebcowen@gmail.com>
2019-01-25 16:44:51 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import JSMpeg from 'jsmpeg-player'
 
 class App extends Component {
+  componentDidMount(){
+    new JSMpeg.VideoElement('#App', 'http://localhost:8082')
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="App">
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
